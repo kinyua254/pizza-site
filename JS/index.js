f4f7eb976229e1e0d49c9495acee0ff10191ac7c
kinyua254 <victor.mutugi@student.moringaschool.com>
2021-11-27 22:42:39 +0300

    order calculations taken

--- JS/index.js
+++ JS/index.js
@@ -4,42 +4,42 @@ $(document).ready(function () {
         event.preventDefault();
 
         var pizzaSize = $("select#size option:selected").val();
-        var pizzaTopping = $("select#size option:selected").val();
-        var pizzaCrust = $("select#size option:selected").val();
+        var pizzaTopping = $("select#toppings option:selected").val();
+        var pizzaCrust = $("select#crust option:selected").val();
         var pizzaNumber = $("input#number").val();
 
-        function pizza(size, topping, crust, number){
+        function Pizza(size, topping, crust, number) {
             this.size = size;
             this.topping = topping;
             this.crust = crust;
             this.number = number;
         }
-        var pizzaNew = new pizza(pizzaSize, pizzaTopping, pizzaCrust, pizzaNumber);
+        var pizzaNew = new Pizza(pizzaSize, pizzaTopping, pizzaCrust, pizzaNumber);
 
         var sizePrice = 0;
         var toppingPrice = 0;
         var crustPrice = 0;
 
-        if(pizzaNew.size == "Small"){
-            sizePrice +=400;
-        }else if(pizzaNew.size == "Medium"){
-            sizePrice +=800;
-        }else if (pizzaNew.size == "Large"){
-            sizePrice +=1100;
+        if (pizzaNew.size == "Small") {
+            sizePrice += 400;
+        } else if (pizzaNew.size == "Medium") {
+            sizePrice += 800;
+        } else if (pizzaNew.size == "Large") {
+            sizePrice += 1100;
         }
-        
-        if(pizzaNew.topping == "BBQ-Chicken" || pizzaNew.topping == "beef" || pizzaNew.topping == "bacon"){
-            toppingPrice +=200;
 
-        }else if(pizzaNew.topping == "vegan" || pizzaNew.topping == "cheese"){
+        if (pizzaNew.topping == "BBQ-Chicken" || pizzaNew.topping == "Beef" || pizzaNew.topping == "Bacon" || pizzaNew.topping == "Ham") {
+            toppingPrice += 200;
+
+        } else if (pizzaNew.topping == "Vegan" || pizzaNew.topping == "Cheese") {
             toppingPrice += 150;
         }
 
-        if(pizzaNew.crust == "crispy"){
+        if (pizzaNew.crust == "Crispy") {
             crustPrice += 80;
-        }else if(pizzaNew.crust == "stuffed"){
+        } else if (pizzaNew.crust == "Stuffed") {
             crustPrice += 75;
-        }else if(pizzaNew.crust == "glutten-free"){
+        } else if (pizzaNew.crust == "Glutten-free") {
             crustPrice += 70;
         }
 
